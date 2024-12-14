1b238f79edd84bedc1c5261c844584c16d754f2a
Hima Sekhar v <himasekhar9963@gmail.com>
2024-12-14 18:08:59 +0530

    New Commit

--- src/test/java/LOOPS/Loops.java
+++ src/test/java/LOOPS/Loops.java
@@ -2,18 +2,8 @@ package LOOPS;
 
 public class Loops {
     public static void main(String[] args) {
-        int square,n,i;
-        n =2;
-        i =1;
-        square=1;
-        while (i<=10) {
-            square = i*n;
-            System.out.println(n+" * "+i+" = " +square);
+        for(char ch='a';ch<='z';ch++){
 
-            i++;
         }
-
     }
-
 }
-
--- src/test/java/windowhandling1.java
+++ src/test/java/windowhandling1.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.Set;
 
@@ -11,9 +12,12 @@ public class windowhandling1 {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver;
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");  // Run in headless mode if you don't need the browser UI
+        driver = new ChromeDriver(options);
         String url = "https://www.amazon.in/";
         driver.get(url);
+        Thread.sleep(10000);
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone 16 pro", Keys.ENTER);
         Thread.sleep(3000);
         driver.findElement(By.xpath("(//span[text()='Apple iPhone 16 Pro (128 GB) - Natural Titanium'])[2]")).click();
